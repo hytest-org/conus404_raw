a79bb1248d45a67733ea399914d49c2a87d363bb
Parker Norton <pnorton@usgs.gov>
2024-11-06 13:10:24 -0700

    Change name from conus404-raw to conus404_raw

--- conus404_raw/version.py
+++ conus404_raw/version.py
@@ -3,7 +3,7 @@ minor = 2
 micro = 0
 __version__ = f"{major}.{minor}.{micro}"
 
-__pakname__ = "conus404-raw"
+__pakname__ = "conus404_raw"
 
 # edit author dictionary as necessary (in order of commits after Bakker and Post)
 author_dict = {
